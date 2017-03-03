00deed25110c3bcddcf2c342c33b42ca3a8e18a2
francisco15 <franciscohuertas38@gmail.com>
2017-03-03 08:48:49 -0500

    creadas vistas de Questions y completado controlador Questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -10,5 +10,50 @@ class QuestionsController < ApplicationController
       @questions = Question.all
     end
   end
+
+  def show
+    @answers = @question.answers
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to question_path(id: @question.id)
+    else
+      flash[:alert] = "The question has not been created, due an error"
+      render :new
+    end
+  end
+
+  def update
+    if @question.update(question_params)
+      redirect_to question_path
+    else
+      flash[:alert] = "The question has not been edited, due an error"
+    end
+  end
+
+  def destroy
+    @question.destroy
+    redirect_to questions_path
+  end
+
+  private
   
+    def set_question
+      @question = Question.find(params[:id])
+    end
+
+    def set_ransack
+      @search = Question.search(params[:q])
+    end
+
+    def question_params
+      params.require(:question).permit(:title, :content, :user_id)
+    end
+
 end
